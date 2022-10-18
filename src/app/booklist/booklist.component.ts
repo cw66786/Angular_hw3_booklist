4b6565f72500c1ee9aad6f04129d4bdd65bb352c
Craig Wilson <cw66786@gmail.com>
2022-10-18 16:55:47 -0500

    fixed css to scss whew that was alot

--- src/app/booklist/booklist.component.ts
+++ src/app/booklist/booklist.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-booklist',
   templateUrl: './booklist.component.html',
-  styleUrls: ['./booklist.component.css']
+  styleUrls: ['./booklist.component.scss']
 })
 export class BooklistComponent implements OnInit {
 
